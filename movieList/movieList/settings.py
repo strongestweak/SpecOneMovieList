a29d853de4dd16175da2cd432ef607011245efa2
MVP\arnie.chipe <akchipe@gmail.com>
2018-09-24 16:27:20 +0800

    Database set up
    MySQL  set up

--- movieList/movieList/settings.py
+++ movieList/movieList/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'movieList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movieList',
+        'USER': 'root',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
+
+
 
 
 # Password validation
