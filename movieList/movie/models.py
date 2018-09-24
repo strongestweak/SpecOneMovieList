484a04b802ae6cdc9487a4863be175118562cc39
MVP\arnie.chipe <akchipe@gmail.com>
2018-09-24 16:52:43 +0800

    createmodel

--- movieList/movie/models.py
+++ movieList/movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+
+	title = models.CharField(max_length=100, unique=True)
+	summary = models.TextField()
+	date_screened = models.DateField()
+	# director = models.ManyToManyField(Director) can evolve
+	director = models.CharField(max_length=100)
+
+	likes = models.PositiveIntegerField(default=0)
+
+	is_active = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	last_updated = models.DateTimeField(auto_now=True)
